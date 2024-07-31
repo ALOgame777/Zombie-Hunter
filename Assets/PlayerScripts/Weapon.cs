0956defee247d30a28f210a5ad125ddc5ccdf65e
ALOgame777 <gl7273@naver.com>
2024-07-31 20:21:46 +0900

    이펙트 추가

--- Assets/PlayerScripts/Weapon.cs
+++ Assets/PlayerScripts/Weapon.cs
@@ -8,7 +8,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100.0f;
     [SerializeField] int damage = 50;
-
+    public ParticleSystem muzzle;
+    public GameObject hitEffet;
     // Update is called once per frame
     void Update()
     {
@@ -20,10 +21,21 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRayCast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzle.Play();
+    }
+
+    private void ProcessRayCast()
+    {
         RaycastHit hit;
         if( Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            
+            CreateHitImpact(hit);
             EnemyFSM target = hit.transform.GetComponent<EnemyFSM>();
             BOSS target1 = hit.transform.GetComponent<BOSS>();
             if (target != null)
@@ -42,5 +54,11 @@ public class Weapon : MonoBehaviour
         }
 
         
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffet, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 4);
     }
 }
