14ff24be48aa9ca3cf7729f8ef05f12e06643b9e
ALOgame777 <gl7273@naver.com>
2024-08-12 10:54:47 +0900

    데미지 애니메이션 폰트 크기 수정

--- Assets/Scripts/PlayerScripts/DamagePopUPAnimation.cs
+++ Assets/Scripts/PlayerScripts/DamagePopUPAnimation.cs
@@ -8,6 +8,8 @@ public class DamagePopUPAnimation : MonoBehaviour
     public AnimationCurve opacityCurve;
     public AnimationCurve scaleCurve;
     public AnimationCurve heightcurve;
+    public AnimationCurve fontSizeCurve; // 새로운 AnimationCurve 추가
+    public float baseFontSize = 23f; // 기본 폰트 크기 설정
 
     private TextMeshProUGUI tmp;
     private float time = 0;
@@ -25,6 +27,8 @@ public class DamagePopUPAnimation : MonoBehaviour
         tmp.color = new Color(1, 1, 1, opacityCurve.Evaluate(time));
         transform.localScale= Vector3.one * scaleCurve.Evaluate(time);
         transform.position = origin + new Vector3(0, 1 + heightcurve.Evaluate(time), 0);
+        // 폰트 크기 업데이트
+        tmp.fontSize = baseFontSize * fontSizeCurve.Evaluate(time);
         time += Time.deltaTime;
     }
 }
